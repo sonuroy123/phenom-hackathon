7e14e786770a35012881536662e07bd0b049be88
sonu <roysonu381@gmail.com>
2025-06-21 15:35:53 +0530

    Initial commit

---
+++ src/main/java/com/phenom/flat_mate/FlatMateApplication.java
@@ -0,0 +1,13 @@
+package com.phenom.flat_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatMateApplication.class, args);
+	}
+
+}
